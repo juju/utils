95402e4d84c05f124d7599e79d45df55f7a19a26
John Weldon <johnweldon4@gmail.com>
2014-12-02 09:22:42 -0800

    One more test

--- uuid_test.go
+++ uuid_test.go
@@ -39,6 +39,7 @@ func (*uuidSuite) TestIsValidUUIDFailsWhenNotValid(c *gc.C) {
 	c.Check(utils.IsValidUUIDString("blah"), gc.Equals, false)
 	c.Check(utils.IsValidUUIDString("blah-9f484882-2f18-4fd2-967d-db9663db7bea"), gc.Equals, false)
 	c.Check(utils.IsValidUUIDString("9f484882-2f18-4fd2-967d-db9663db7bea-blah"), gc.Equals, false)
+	c.Check(utils.IsValidUUIDString("9f484882-2f18-4fd2-967d-db9663db7bea"), gc.Equals, true)
 }
 
 func (*uuidSuite) TestUUIDFromString(c *gc.C) {

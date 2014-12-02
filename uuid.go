65f633e161736bf8f6d1c1ab8cf9143d0ba9a0e2
John Weldon <johnweldon4@gmail.com>
2014-12-02 09:20:03 -0800

    Fix un-anchored IsValidUUIDString regex

--- uuid.go
+++ uuid.go
@@ -15,7 +15,7 @@ import (
 // UUID represent a universal identifier with 16 octets.
 type UUID [16]byte
 
-var validUUID = regexp.MustCompile("[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[8,9,a,b][0-9a-f]{3}-[0-9a-f]{12}")
+var validUUID = regexp.MustCompile("^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[8,9,a,b][0-9a-f]{3}-[0-9a-f]{12}$")
 
 func UUIDFromString(s string) (UUID, error) {
 	if !IsValidUUIDString(s) {

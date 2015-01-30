f68f941b4f017e3042d2ffa5ea8eedd17daf7f29
Eric Snow <ericsnowcurrently@gmail.com>
2015-01-30 10:15:12 -0700

    Update CachedOps.

--- fs/cachedops.go
+++ fs/cachedops.go
@@ -10,53 +10,80 @@ import (
 
 // CachedOps is an implementation of Operations that records each
 // requested operation. It also maintains a simple in-memory filesystem.
+// Both features make CachedOps useful in testing.
 type CachedOps struct {
-	*SimpleOps
+	ops    Operations
 	Events *OpEvents
 }
 
 // NewCachedOps initializes a new CachedOps and returns it.
-func NewCachedOps() *CachedOps {
+func NewCachedOps(ops Operations) *CachedOps {
 	return &CachedOps{
-		SimpleOps: NewSimpleOps(),
-		Events:    &OpEvents{},
+		ops:    ops,
+		Events: &OpEvents{},
 	}
 }
 
 // Exists implements Operations.
 func (co *CachedOps) Exists(name string) (bool, error) {
-	co.Events.Add(OpExists, name)
-	return co.SimpleOps.Exists(name)
+	co.Events.Record(OpExists, name)
+	return co.ops.Exists(name)
+}
+
+// Info implements Operations.
+func (co *CachedOps) Info(name string) (os.FileInfo, error) {
+	co.Events.Record(OpInfo, name)
+	return co.ops.Info(name)
 }
 
 // MkdirAll implements Operations.
-func (co *CachedOps) MkdirAll(dirname string, mode os.FileMode) error {
-	event := co.Events.Add(OpExists, dirname)
-	event.Mode = mode
-	return co.SimpleOps.MkdirAll(dirname, mode)
+func (co *CachedOps) MkdirAll(dirname string, perm os.FileMode) error {
+	event := co.Events.Record(OpMkdirAll, dirname)
+	event.Permissions = perm
+	return co.ops.MkdirAll(dirname, perm)
+}
+
+// ListDir implements Operations.
+func (co *CachedOps) ListDir(dirname string) ([]os.FileInfo, error) {
+	co.Events.Record(OpListDir, dirname)
+	return co.ops.ListDir(dirname)
 }
 
 // ReadFile implements Operations.
 func (co *CachedOps) ReadFile(filename string) ([]byte, error) {
-	co.Events.Add(OpExists, filename)
-	return co.SimpleOps.ReadFile(filename)
+	co.Events.Record(OpReadFile, filename)
+	return co.ops.ReadFile(filename)
 }
 
 // CreateFile implements Operations.
 func (co *CachedOps) CreateFile(filename string) (io.WriteCloser, error) {
-	co.Events.Add(OpExists, filename)
-	return co.SimpleOps.CreateFile(filename)
+	co.Events.Record(OpCreateFile, filename)
+	return co.ops.CreateFile(filename)
+}
+
+// WriteFile implements Operations.
+func (co *CachedOps) WriteFile(filename string, data []byte, perm os.FileMode) error {
+	event := co.Events.Record(OpWriteFile, filename)
+	event.Permissions = perm
+	return co.ops.WriteFile(filename, data, perm)
 }
 
 // RemoveAll implements Operations.
 func (co *CachedOps) RemoveAll(name string) error {
-	co.Events.Add(OpExists, name)
-	return co.SimpleOps.RemoveAll(name)
+	co.Events.Record(OpRemoveAll, name)
+	return co.ops.RemoveAll(name)
 }
 
 // Chmod implements Operations.
-func (co *CachedOps) Chmod(name string, mode os.FileMode) error {
-	event := co.Events.Add(OpChmod, name)
-	event.Mode = mode
-	return co.SimpleOps.Chmod(name, mode)
+func (co *CachedOps) Chmod(name string, perm os.FileMode) error {
+	event := co.Events.Record(OpChmod, name)
+	event.Permissions = perm
+	return co.ops.Chmod(name, perm)
+}
+
+// Symlink implements Operations.
+func (co *CachedOps) Symlink(oldName, newName string) error {
+	event := co.Events.Record(OpSymlink, newName)
+	event.Source = oldName
+	return co.ops.Symlink(oldName, newName)
 }

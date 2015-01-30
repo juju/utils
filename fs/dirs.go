6acea20436ba65efe8393cb43df3a59331dc5851
Eric Snow <ericsnowcurrently@gmail.com>
2015-01-30 10:16:01 -0700

    Add a doc comment to ListSubdirectories.

--- fs/dirs.go
+++ fs/dirs.go
@@ -9,7 +9,13 @@ import (
 	"github.com/juju/errors"
 )
 
+// ListSubdirectories extracts the names of all subdirectories of the
+// specified directory and returns that list.
 func ListSubdirectories(dirname string) ([]string, error) {
+	return listSubdirectories(dirname, &Ops{})
+}
+
+func listSubdirectories(dirname string, fops Operations) ([]string, error) {
 	entries, err := ioutil.ReadDir(dirname)
 	if err != nil {
 		return nil, errors.Trace(err)

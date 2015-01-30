3f55f355b5ff8c7c37448314ef82019ea73ec4c0
Eric Snow <ericsnowcurrently@gmail.com>
2015-01-30 13:38:23 -0700

    Expose ListSubdirectoriesOp.

--- fs/dirs.go
+++ fs/dirs.go
@@ -4,19 +4,20 @@
 package fs
 
 import (
-	"io/ioutil"
-
 	"github.com/juju/errors"
 )
 
 // ListSubdirectories extracts the names of all subdirectories of the
 // specified directory and returns that list.
 func ListSubdirectories(dirname string) ([]string, error) {
-	return listSubdirectories(dirname, &Ops{})
+	return ListSubdirectoriesOp(dirname, &Ops{})
 }
 
-func listSubdirectories(dirname string, fops Operations) ([]string, error) {
-	entries, err := ioutil.ReadDir(dirname)
+// ListSubdirectoriesOp extracts the names of all subdirectories of the
+// specified directory and returns that list. The provided Operations
+// is used to make the filesystem calls.
+func ListSubdirectoriesOp(dirname string, fops Operations) ([]string, error) {
+	entries, err := fops.ListDir(dirname)
 	if err != nil {
 		return nil, errors.Trace(err)
 	}

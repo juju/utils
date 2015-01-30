f6b26f26e3a67fd4b405df19b73e30431f14fcb4
Eric Snow <ericsnowcurrently@gmail.com>
2015-01-30 10:17:53 -0700

    Update Operations and Ops.

--- fs/ops.go
+++ fs/ops.go
@@ -7,6 +7,8 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+
+	"github.com/juju/utils/symlink"
 )
 
 // Operations exposes various key file system operations as methods
@@ -17,8 +19,15 @@ type Operations interface {
 	// checking the error with os.IsNotExist.
 	Exists(name string) (bool, error)
 
+	// Info is a replacement for os.Stat. However, use Exists if you
+	// are only checking the error return (e.g. os.IsNotExist).
+	Info(name string) (os.FileInfo, error)
+
 	// MkdirAll is a replacement for os.MkdirAll.
-	MkdirAll(dirname string) error
+	MkdirAll(dirname string, perm os.FileMode) error
+
+	// ListDir is a replacement for ioutil.ReadDir.
+	ListDir(dirname string) ([]os.FileInfo, error)
 
 	// ReadFile is a replacement for ioutil.ReadFile.
 	ReadFile(filename string) ([]byte, error)
@@ -26,16 +35,22 @@ type Operations interface {
 	// CreateFile is a replacement for os.Create.
 	CreateFile(filename string) (io.WriteCloser, error)
 
+	// WriteFile is a replacement for ioutil.WriteFile.
+	WriteFile(filename string, data []byte, perm os.FileMode) error
+
 	// RemoveAll is a replacement for os.RemoveAll.
 	RemoveAll(name string) error
 
 	// Chmod is a replacement for os.Chmod.
 	Chmod(name string, mode os.FileMode) error
+
+	// Symlink is a replacement for utils/symlink.New.
+	Symlink(oldName, newName string) error
 }
 
-// Ops satisfies the FileOperations interface, wrapping the
+// Ops satisfies the Operations interface, wrapping the
 // equivalent functionality out of the Go stdlib (e.g os.MkdirAll
-// for FileOperations.MkdirAll).
+// for FileOperations.MkdirAll) and of other relevant packages.
 type Ops struct{}
 
 // Exists implements Operations.
@@ -50,9 +65,19 @@ func (Ops) Exists(name string) (bool, error) {
 	return true, nil
 }
 
+// Info implements Operations.
+func (Ops) Info(name string) (os.FileInfo, error) {
+	return os.Stat(name)
+}
+
 // MkdirAll implements Operations.
-func (Ops) MkdirAll(dirname string, mode os.FileMode) error {
-	return os.MkdirAll(dirname, mode)
+func (Ops) MkdirAll(dirname string, perm os.FileMode) error {
+	return os.MkdirAll(dirname, perm)
+}
+
+// ListDir implements Operations.
+func (Ops) ListDir(dirname string) ([]os.FileInfo, error) {
+	return ioutil.ReadDir(dirname)
 }
 
 // ReadFile implements Operations.
@@ -65,6 +90,11 @@ func (Ops) CreateFile(filename string) (io.WriteCloser, error) {
 	return os.Create(filename)
 }
 
+// WriteFile implements Operations.
+func (Ops) WriteFile(filename string, data []byte, perm os.FileMode) error {
+	return ioutil.WriteFile(filename, data, perm)
+}
+
 // RemoveAll implements Operations.
 func (Ops) RemoveAll(name string) error {
 	return os.RemoveAll(name)
@@ -73,4 +103,9 @@ func (Ops) RemoveAll(name string) error {
 // Chmod implements Operations.
 func (Ops) Chmod(name string, mode os.FileMode) error {
 	return os.Chmod(name, mode)
+}
+
+// Symlink implements Operations.
+func (Ops) Symlink(oldName, newName string) error {
+	return symlink.New(oldName, newName)
 }

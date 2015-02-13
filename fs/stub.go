20253b94147d1310f315912bce543ac502cf9f57
Eric Snow <ericsnowcurrently@gmail.com>
2015-02-13 14:03:15 -0700

    Fake -> Stub.

--- fs/stub.go
+++ fs/stub.go
@@ -10,46 +10,46 @@ import (
 	"github.com/juju/testing"
 )
 
-// FakeFile keeps track of calls to io.ReadWriteCloser methods and
+// StubFile keeps track of calls to io.ReadWriteCloser methods and
 // allows direct control of what those methods return. This is useful
-// in tests. The calls are tracked in testing.Fake. The error return
+// in tests. The calls are tracked in testing.Stub. The error return
 // values are also managed there.
-type FakeFile struct {
-	// This is a pointer so it may be shared between different fakes.
-	*testing.Fake
+type StubFile struct {
+	// This is a pointer so it may be shared between different stubs.
+	*testing.Stub
 
 	// NWritten is the value that will be returned by Read() and Write().
 	NWritten int
 }
 
-// NewFakeFile builds a new FakeFile and returns it.
-func NewFakeFile() *FakeFile {
-	fake := &FakeFile{
-		Fake: &testing.Fake{},
+// NewStubFile builds a new StubFile and returns it.
+func NewStubFile() *StubFile {
+	stub := &StubFile{
+		Stub: &testing.Stub{},
 	}
-	return fake
+	return stub
 }
 
 // Read Implements io.Reader.
-func (ff *FakeFile) Read(buf []byte) (int, error) {
+func (ff *StubFile) Read(buf []byte) (int, error) {
 	ff.AddCall("Read", buf)
 	return ff.NWritten, ff.NextErr()
 }
 
 // Write Implements io.Writer.
-func (ff *FakeFile) Write(data []byte) (int, error) {
+func (ff *StubFile) Write(data []byte) (int, error) {
 	ff.AddCall("Write", data)
 	return ff.NWritten, ff.NextErr()
 }
 
 // Close Implements io.Closer.
-func (ff *FakeFile) Close() error {
+func (ff *StubFile) Close() error {
 	ff.AddCall("Close")
 	return ff.NextErr()
 }
 
-// FakeOpsReturns holds all the return values for the FakeOps methods.
-type FakeOpsReturns struct {
+// StubOpsReturns holds all the return values for the StubOps methods.
+type StubOpsReturns struct {
 	// Exists is the value that will be returned from Exists().
 	Exists bool
 
@@ -69,88 +69,88 @@ type FakeOpsReturns struct {
 	File io.WriteCloser
 }
 
-// FakeOps keeps track of calls to Operation methods and allows direct
+// StubOps keeps track of calls to Operation methods and allows direct
 // control of what those methods return. This is useful in tests. The
-// calls are tracked in testing.Fake. The error return values are also
+// calls are tracked in testing.Stub. The error return values are also
 // managed there.
-type FakeOps struct {
-	// This is a pointer so it may be shared between different fakes.
-	*testing.Fake
+type StubOps struct {
+	// This is a pointer so it may be shared between different stubs.
+	*testing.Stub
 
-	// Returns holds the fake's (non-error) return values.
-	Returns FakeOpsReturns
+	// Returns holds the stub's (non-error) return values.
+	Returns StubOpsReturns
 }
 
-// NewFakeOps builds a new FakeOps and returns it.
-func NewFakeOps() *FakeOps {
-	fake := &FakeOps{
-		Fake: &testing.Fake{},
+// NewStubOps builds a new StubOps and returns it.
+func NewStubOps() *StubOps {
+	stub := &StubOps{
+		Stub: &testing.Stub{},
 	}
-	return fake
+	return stub
 }
 
 // Exists implements Operations.
-func (fo *FakeOps) Exists(name string) (bool, error) {
+func (fo *StubOps) Exists(name string) (bool, error) {
 	fo.AddCall("Exists", name)
 	return fo.Returns.Exists, fo.NextErr()
 }
 
 // Info implements Operations.
-func (fo *FakeOps) Info(name string) (os.FileInfo, error) {
+func (fo *StubOps) Info(name string) (os.FileInfo, error) {
 	fo.AddCall("Info", name)
 	return fo.Returns.Info, fo.NextErr()
 }
 
 // MkdirAll implements Operations.
-func (fo *FakeOps) MkdirAll(dirname string, perm os.FileMode) error {
+func (fo *StubOps) MkdirAll(dirname string, perm os.FileMode) error {
 	fo.AddCall("MkdirAll", dirname, perm)
 	return fo.NextErr()
 }
 
 // ListDir implements Operations.
-func (fo *FakeOps) ListDir(dirname string) ([]os.FileInfo, error) {
+func (fo *StubOps) ListDir(dirname string) ([]os.FileInfo, error) {
 	fo.AddCall("ReadDir", dirname)
 	return fo.Returns.DirEntries, fo.NextErr()
 }
 
 // ReadFile implements Operations.
-func (fo *FakeOps) ReadFile(filename string) ([]byte, error) {
+func (fo *StubOps) ReadFile(filename string) ([]byte, error) {
 	fo.AddCall("ReadFile", filename)
 	return fo.Returns.Data, fo.NextErr()
 }
 
 // CreateFile implements Operations.
-func (fo *FakeOps) CreateFile(filename string) (io.WriteCloser, error) {
+func (fo *StubOps) CreateFile(filename string) (io.WriteCloser, error) {
 	fo.AddCall("CreateFile", filename)
 	return fo.Returns.File, fo.NextErr()
 }
 
 // WriteFile implements Operations.
-func (fo *FakeOps) WriteFile(filename string, data []byte, perm os.FileMode) error {
+func (fo *StubOps) WriteFile(filename string, data []byte, perm os.FileMode) error {
 	fo.AddCall("WriteFile", filename, data, perm)
 	return fo.NextErr()
 }
 
 // RemoveAll implements Operations.
-func (fo *FakeOps) RemoveAll(name string) error {
+func (fo *StubOps) RemoveAll(name string) error {
 	fo.AddCall("RemoveAll", name)
 	return fo.NextErr()
 }
 
 // Chmod implements Operations.
-func (fo *FakeOps) Chmod(name string, perm os.FileMode) error {
+func (fo *StubOps) Chmod(name string, perm os.FileMode) error {
 	fo.AddCall("Chmod", name, perm)
 	return fo.NextErr()
 }
 
 // Symlink implements Operations.
-func (fo *FakeOps) Symlink(oldName, newName string) error {
+func (fo *StubOps) Symlink(oldName, newName string) error {
 	fo.AddCall("Symlink", oldName, newName)
 	return fo.NextErr()
 }
 
 // ReadLink implements Operations.
-func (fo *FakeOps) Readlink(name string) (string, error) {
+func (fo *StubOps) Readlink(name string) (string, error) {
 	fo.AddCall("Readlink", name)
 	return fo.Returns.Filename, fo.NextErr()
 }

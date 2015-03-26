d85065b8effca196049d646e5316948a12e78281
aznashwan <aznashwan@yahoo.com>
2015-03-26 16:50:42 +0200

    Removed old apt package.

--- apt/export_test.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2014 Canonical Ltd.
-// Licensed under the LGPLv3, see LICENCE file for details.
-
-package apt
-
-var (
-	ProcessStateSys        = &processStateSys
-	InstallAttemptStrategy = &installAttemptStrategy
-)

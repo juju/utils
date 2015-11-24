999c3e6bfdaedd88850f9307463dd3804010d600
James Tunnicliffe <james.tunnicliffe@nanosheep.org>
2015-11-24 09:21:02 +0000

    Fix race.

--- fslock/fslock.go
+++ fslock/fslock.go
@@ -122,6 +122,7 @@ func NewLock(lockDir, name string, cfg LockConfig) (*Lock, error) {
 	if err := os.RemoveAll(lock.aliveFile(lock.PID)); err != nil {
 		return nil, err
 	}
+	lock.stopWritingAliveFile = make(chan struct{})
 	return lock, nil
 }
 
@@ -157,8 +158,6 @@ func (lock *Lock) isAlive(PID int) bool {
 // createAliveFile kicks off a gorouteine that creates a proof of life file
 // and keeps its timestamp current.
 func (lock *Lock) createAliveFile(dir string) {
-	lock.stopWritingAliveFile = make(chan struct{})
-
 	go func() {
 		aliveFile := lock.aliveFile(lock.PID)
 		for {

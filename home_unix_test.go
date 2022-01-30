25627f8eb422d085d78cf4cff976e68ae5a5dc99
Ian Booth <ian.booth@canonical.com>
2022-01-31 08:46:43 +1000

    Home() returns real home, not snap home

--- home_unix_test.go
+++ home_unix_test.go
@@ -1,5 +1,6 @@
 // Copyright 2011, 2012, 2013 Canonical Ltd.
 // Licensed under the LGPLv3, see LICENCE file for details.
+//go:build !windows
 // +build !windows
 
 package utils_test
@@ -20,5 +21,12 @@ var _ = gc.Suite(&homeSuite{})
 func (s *homeSuite) TestHomeLinux(c *gc.C) {
 	h := "/home/foo/bar"
 	s.PatchEnvironment("HOME", h)
+	c.Check(utils.Home(), gc.Equals, h)
+}
+
+func (s *homeSuite) TestHomeConfined(c *gc.C) {
+	h := "/home/foo/bar"
+	s.PatchEnvironment("HOME", "/home/user/snap/foo/1")
+	s.PatchEnvironment("SNAP_REAL_HOME", h)
 	c.Check(utils.Home(), gc.Equals, h)
 }
